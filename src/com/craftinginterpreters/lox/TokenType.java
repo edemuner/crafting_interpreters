ffb526df6e78dabb4d6e05df3bd129682b7b40f6
edemuner <eduardood15@hotmail.com>
2024-07-17 20:57:55 -0300

    add Token.java with the basic structore of a Token

--- src/com/craftinginterpreters/lox/TokenType.java
+++ src/com/craftinginterpreters/lox/TokenType.java
@@ -1,16 +1,16 @@
 package com.craftinginterpreters.lox;
 
-    enum TokenType {
+enum TokenType {
 
-        // single character tokens
-        LEFT_PAREN, RIGHT_PAREN, LEFT_BRACE, RIGHT_BRACE, COMMA, DOT, MINUS, PLUS, SEMICOLON, SLASH, STAR,
+    // single character tokens
+    LEFT_PAREN, RIGHT_PAREN, LEFT_BRACE, RIGHT_BRACE, COMMA, DOT, MINUS, PLUS, SEMICOLON, SLASH, STAR,
 
-        // one or two character tokens
-        BANG, BANG_EQUAL, EQUAL, EQUAL_EQUAL, GREATER, GREATER_EQUAL, LESS, LESS_EQUAL,
+    // one or two character tokens
+    BANG, BANG_EQUAL, EQUAL, EQUAL_EQUAL, GREATER, GREATER_EQUAL, LESS, LESS_EQUAL,
 
-        // literals
-        IDENTIFIER, STRING, NUMBER,
+    // literals
+    IDENTIFIER, STRING, NUMBER,
 
-        // keywords
-        AND, CLASS, ELSE, FALSE, FUN, FOR, IF, NIL, OR, PRINT, RETURN, SUPER, THIS, TRUE, VAR, WHILE, EOF
-    }
+    // keywords
+    AND, CLASS, ELSE, FALSE, FUN, FOR, IF, NIL, OR, PRINT, RETURN, SUPER, THIS, TRUE, VAR, WHILE, EOF
+}
