462ddf19c08515880439b232b9f39396f33b0828
edemuner <eduardood15@hotmail.com>
2024-07-20 12:54:18 -0300

    add logic to handle lexemes with 1 or 2 chars, it renders one or another token depending on the next one, add helper method match(char expected) to verify the next one

--- src/com/craftinginterpreters/lox/Scanner.java
+++ src/com/craftinginterpreters/lox/Scanner.java
@@ -53,7 +53,24 @@ public class Scanner {
             break;
             case '*' : addToken(STAR);
             break;
+            case '!' : addToken(match('=') ? BANG_EQUAL : BANG);
+            break;
+            case '=' : addToken(match('=') ? EQUAL_EQUAL : EQUAL);
+            break;
+            case '<' : addToken(match('=') ? LESS_EQUAL : LESS);
+            break;
+            case '>' : addToken(match('=') ? GREATER_EQUAL : GREATER);
+            default: Lox.error(line, "Unexpected character");
+            break;
+
         }
+    }
+
+    private boolean match(char expected){
+        if(isAtEnd()) return false;
+        if(source.charAt(current) != expected) return false;
+        current ++;
+        return true;
     }
 
     private boolean isAtEnd(){
