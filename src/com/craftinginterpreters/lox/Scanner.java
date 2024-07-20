9b69c84eb1025d4bbcf748f8371ae15f4d3ae565
edemuner <eduardood15@hotmail.com>
2024-07-20 18:02:25 -0300

    adds a handler to / and white spaces into scanToken()

--- src/com/craftinginterpreters/lox/Scanner.java
+++ src/com/craftinginterpreters/lox/Scanner.java
@@ -33,36 +33,32 @@ public class Scanner {
     private void scanToken(){
         char c = advance();
         switch(c){
-            case '(' : addToken(LEFT_PAREN);
-            break;
-            case ')' : addToken(RIGHT_PAREN);
-            break;
-            case '{' : addToken(LEFT_BRACE);
-            break;
-            case '}' : addToken(RIGHT_BRACE);
-            break;
-            case ',' : addToken(COMMA);
-            break;
-            case '.' : addToken(DOT);
-            break;
-            case '-' : addToken(MINUS);
-            break;
-            case '+' : addToken(PLUS);
-            break;
-            case ';' : addToken(SEMICOLON);
-            break;
-            case '*' : addToken(STAR);
-            break;
-            case '!' : addToken(match('=') ? BANG_EQUAL : BANG);
-            break;
-            case '=' : addToken(match('=') ? EQUAL_EQUAL : EQUAL);
-            break;
-            case '<' : addToken(match('=') ? LESS_EQUAL : LESS);
-            break;
-            case '>' : addToken(match('=') ? GREATER_EQUAL : GREATER);
-            default: Lox.error(line, "Unexpected character");
-            break;
-
+            case '(' : addToken(LEFT_PAREN); break;
+            case ')' : addToken(RIGHT_PAREN); break;
+            case '{' : addToken(LEFT_BRACE); break;
+            case '}' : addToken(RIGHT_BRACE); break;
+            case ',' : addToken(COMMA); break;
+            case '.' : addToken(DOT); break;
+            case '-' : addToken(MINUS); break;
+            case '+' : addToken(PLUS); break;
+            case ';' : addToken(SEMICOLON); break;
+            case '*' : addToken(STAR); break;
+            case '!' : addToken(match('=') ? BANG_EQUAL : BANG); break;
+            case '=' : addToken(match('=') ? EQUAL_EQUAL : EQUAL); break;
+            case '<' : addToken(match('=') ? LESS_EQUAL : LESS); break;
+            case '>' : addToken(match('=') ? GREATER_EQUAL : GREATER); break;
+            case '/' :
+                if(match('/')){
+                    // a comment goes until the end of the line
+                    while(peek() != '\n' && !isAtEnd()) advance();
+                } else {
+                    addToken(SLASH);
+                }
+            case '\t' :
+            case '\r' :
+            case ' ' :
+            case '\n' : line++; break;
+            default: Lox.error(line, "Unexpected character"); break;
         }
     }
 
@@ -71,6 +67,11 @@ public class Scanner {
         if(source.charAt(current) != expected) return false;
         current ++;
         return true;
+    }
+
+    private char peek(){
+        if(isAtEnd()) return '\0';
+        return source.charAt(current);
     }
 
     private boolean isAtEnd(){
